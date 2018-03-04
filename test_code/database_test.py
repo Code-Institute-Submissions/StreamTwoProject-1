e70389dd27cb0ab84b2f5766062a8054c1213f8a
Justin Robinson <kilik2021@users.noreply.github.com>
2018-03-04 10:24:26 +0000

    added additional code to database_test to converting data to json

--- test_code/database_test.py
+++ test_code/database_test.py
@@ -1,4 +1,5 @@
 import pymongo
+import json
 
 # function to connect to the mongoDB
 def mongo_connect():
@@ -11,18 +12,56 @@ def mongo_connect():
 
 
 # Connect to the database.
-conn = mongo_connect()
-db = conn['donorsMA']
-collection = db['projects']
+#conn = mongo_connect()
+#db = conn['donorsMA']
+#collection = db['projects']
 
 # print a list of databases
-print "Name of available Databases %s \n\n" % conn.database_names()
+#print "Name of available Databases %s \n\n" % conn.database_names()
 
 # get record count
-print "Number of Records in 'projects' collection: %s \n\n" % collection.count()
+#print "Number of Records in 'projects' collection: %s \n\n" % collection.count()
 
 # print a list of fields
-print "Fields Names in 'projects' collection: \n"
-record = collection.find_one()
-for key in record:
-	print key
+#print "Fields Names in 'projects' collection: \n"
+#record = collection.find_one()
+#for key in record:
+#	print key
+
+
+# declare constant variables
+MONGO_URI = 'mongodb://localhost:27017/'  # local host mongoDB
+DB_NAME = 'donorsMA'
+COLLECTION_NAME = 'projects'
+
+FIELDS = {
+		'_id': False,
+
+		# date fields
+		'date_posted': True,
+		
+		# focus area/subject fields
+		'primary_focus_area': True,
+		
+		# school fields
+		'grade_level' : True, 'school_city': True, 'school_county': True, 
+
+		# project details
+		'resource_type': True, 'poverty_level': True, 'num_donors': True, 
+		'funding_status': True,
+
+		# totals
+		'total_donations': True
+	}
+
+# open a connection to the MongoDB
+with pymongo.MongoClient(MONGO_URI) as conn:
+	# define the collection to access
+	collection = conn[DB_NAME][COLLECTION_NAME]
+
+	# filter the results to only include the fields specified in the FIELDS constant
+	# and limit to the first 22000 records
+	projects = collection.find(projection=FIELDS, limit=22000)
+
+	#print projects.count()
+	print json.dumps(list(projects))
